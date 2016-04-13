7cb1cde7dd515b243329d6843249b5a3f592cd04
Administrador <Administrador@10.8.8.135>
2016-04-12 21:10:05 -0500

    Crear controlador

--- src/main/java/com/uniminuto/edu/ventacar/ctrl/AutoJSFBean.java
+++ src/main/java/com/uniminuto/edu/ventacar/ctrl/AutoJSFBean.java
@@ -3,10 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.uniminuto.edu.ventacar.ctrl;
 
-
-public class AutoJSFBean  {
+public class AutoJSFBean {
 
 }
